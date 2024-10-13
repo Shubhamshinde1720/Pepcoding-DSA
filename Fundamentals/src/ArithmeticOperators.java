8a3da0bcc9b0e52d74bb5101fe64bb1a61f3c8c2
SHUBHAM <shubhamshinde1720@gmail.com>
2024-10-14 02:07:55 +0530

    fouth program

---
+++ Fundamentals/src/ArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperators
+{
+    public static void main(String[] args) {
+        int a=19;
+        int b=5;
+
+        System.out.println("sum of "+a+" and "+b+" is "+(a+b));
+        System.out.println("sub of "+a+" and "+b+" is "+(a-b));
+        System.out.println("mul of "+a+" and "+b+" is "+(a*b));
+        System.out.println("div of "+a+" and "+b+" is "+(a/b));
+        System.out.println("mod of "+a+" and "+b+" is "+(a%b));
+
+
+    }
+}
