16aed1e79cabebbbd6d5fa67704d83ad3d6f1905
SHUBHAM <shubhamshinde1720@gmail.com>
2024-10-14 02:11:50 +0530

    fifth program

---
+++ Fundamentals/src/EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+
+    public static void main(String[] args) {
+        int x=19;
+
+        if(x%2==0)
+            System.out.println("Even");
+        else System.out.println("Odd");
+    }
+}
