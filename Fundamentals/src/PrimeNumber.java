de54b39f4424234cd808373d662d90ef84c54f52
SHUBHAM <shubhamshinde1720@gmail.com>
2024-10-14 23:04:21 +0530

    i Love Java

--- Fundamentals/src/PrimeNumber.java
+++ Fundamentals/src/PrimeNumber.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        Scanner shub=new Scanner(System.in);
-        System.out.println("Enter a number");
-        int n=shub.nextInt();
+        Scanner shub = new Scanner(System.in);
+        System.out.println("Enter a number test cases");
+        int t = shub.nextInt();
 
-        boolean flag=true;
-        for(int i=2;i<n; i++){
-            if(n%i==0){
-                flag=false;
+
+        while (t-- > 0) {
+            int n = shub.nextInt();
+
+            boolean flag = true;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    flag = false;
+                }
             }
+            System.out.println((flag) ? "This is Prime number" : "This is NonPrime Number");
         }
-        System.out.println((flag)?"This is Prime number":"This is NonPrime Number");
     }
 }
